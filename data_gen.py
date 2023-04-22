3ce7c6c2a1f93148eebbe08a0ace0387abbc538c
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-22 14:06:38 +0200

    something worked

--- data_gen.py
+++ data_gen.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-one= [float(x) for x in range(1,101)]
-two= [x+2 + np.random.normal(0,1) for x in range(1,101)]
+one= [float(x) for x in range(1,201)]
+two= [x+2 + np.random.normal(0,1) for x in range(1,201)]
 df = pd.DataFrame({'one':one,'two':two})
 print(df)
 df.to_csv('data.csv',index=False)
