b507fe4e4d7bf38e6e9531ff012111f160d5d299
Jan Kocurek <jas.kocurek2@wp.pl>
2023-05-04 13:22:24 +0200

    batches

--- data_process.py
+++ data_process.py
@@ -1,11 +1,13 @@
 import os
 import pandas as pd
 
+CSV_LABELS = ['reference_distance', 'mesured_distance', 'signal_strength', "delta"]
+
 files = os.listdir(os.getcwd() + "\\pomiary_txt")
 
-
-
 csv_lines = []
+csv_batch = []
+csv_batches = []
 
 for file in files:
     if "txt" in file:
@@ -17,7 +19,14 @@ for file in files:
                     csv_line = [float(file.replace(".txt", "")), float(csv_arr[0]), float(csv_arr[1]), float(csv_arr[0]) - float(file.replace(".txt", ""))]
                     print("Appending line: " + str(csv_line))
                     csv_lines.append(csv_line)
+                    csv_batch.append(csv_line)
+            csv_batches.append((file.replace(".txt", ""), pd.DataFrame(columns=CSV_LABELS, data=csv_batch)))
+            
 
-df = pd.DataFrame(columns=['reference_distance', 'mesured_distance', 'signal_strength', "delta"], data=csv_lines)
 
-df.to_csv('data.csv')
+df = pd.DataFrame(columns=CSV_LABELS, data=csv_lines)
+
+df.to_csv('./pomiary_csv/data_full.csv')
+
+for batch in csv_batches:
+    batch[1].to_csv('./pomiary_csv/' + batch[0] + '.csv')
