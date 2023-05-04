3e66dc4d1de5be2450574ccc6fcb58216d202e84
Jan Kocurek <jas.kocurek2@wp.pl>
2023-05-04 13:36:16 +0200

    fixes float conversion

--- data_process.py
+++ data_process.py
@@ -13,15 +13,16 @@ def load_data_in_batches():
     for file in files:
         if "txt" in file:
             with open(os.path.join(os.getcwd() + "\\pomiary_txt", file), 'r') as f:
+                reference_distance = float(file.replace(".txt", ""))
                 for index, line in enumerate(f.readlines()):
                     if(line != "Timed out!|7000\n"):
                         csv_arr = line.split('|')
                         csv_arr = csv_arr[1:3]
-                        csv_line = [float(file.replace(".txt", "")), float(csv_arr[0]), float(csv_arr[1]), float(csv_arr[0]) - float(file.replace(".txt", ""))]
+                        csv_line = [reference_distance, float(csv_arr[0]), float(csv_arr[1]), float(csv_arr[0]) - float(file.replace(".txt", ""))]
                         print("Appending line: " + str(csv_line))
                         csv_lines.append(csv_line)
                         csv_batch.append(csv_line)
-                csv_batches.append((file.replace(".txt", ""), pd.DataFrame(columns=CSV_LABELS, data=csv_batch)))
+                csv_batches.append((reference_distance, pd.DataFrame(columns=CSV_LABELS, data=csv_batch)))
                 
 
 
@@ -36,4 +37,4 @@ if __name__ == "__main__":
     df.to_csv('./pomiary_csv/data_full.csv')
 
     for batch in csv_batches:
-        batch[1].to_csv('./pomiary_csv/' + batch[0] + '.csv')
+        batch[1].to_csv('./pomiary_csv/' + str(batch[0]) + '.csv')
