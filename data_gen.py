6c1201f349996c014939bd9c50149ac40e6b7445
WojciechPtas <wojciech.ptas@gmail.com>
2023-06-15 15:13:37 +0200

    Minor changes in data gen

--- data_gen.py
+++ data_gen.py
@@ -2,7 +2,7 @@ import pandas as pd
 import math
 import numpy as np
 one= [float(x) for x in range(1,201)]
-two= [x+2 + np.random.normal(0,1) for x in range(1,201)]
+two= [x + 2 + np.random.normal(0,1) for x in range(1,201)]
 df = pd.DataFrame({'one':one,'two':two})
 print(df)
 df.to_csv('data.csv',index=False)
