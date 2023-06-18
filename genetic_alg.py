621a726ce05cd93e0f6ca472b94746fe5fbf8178
WojciechPtas <wojciech.ptas@gmail.com>
2023-06-18 17:25:25 +0200

    Implement genetic algorithm
    To get coefficients for a polynomial

---
+++ genetic_alg.py
@@ -0,0 +1,132 @@
+# genetic algorithm search for continuous function optimization
+from numpy.random import randint
+from numpy.random import rand
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+data = pd.read_csv('data.csv')
+real_distance = data.iloc[:, 1].values
+measured_distance = data.iloc[:, 2].values
+power = data.iloc[:, 3].values
+delta = data.iloc[:, 4].values
+data_X = measured_distance
+data_y = delta
+X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.2, random_state=42)
+# objective function
+def objective(coefficients):
+    global X_train,y_train
+    fitness = 0
+    for i,x in enumerate(X_train):
+        predicted_value = np.polyval(coefficients, x)
+        target_value = y_train[i]
+        fitness += (predicted_value - target_value) ** 2
+
+    return fitness
+
+# decode bitstring to numbers
+def decode(bounds, n_bits, bitstring):
+	decoded = list()
+	largest = 2**n_bits
+	for i in range(len(bounds)):
+		# extract the substring
+		start, end = i * n_bits, (i * n_bits)+n_bits
+		substring = bitstring[start:end]
+		# convert bitstring to a string of chars
+		chars = ''.join([str(s) for s in substring])
+		# convert string to integer
+		integer = int(chars, 2)
+		# scale integer to desired range
+		value = bounds[i][0] + (integer/largest) * (bounds[i][1] - bounds[i][0])
+		# store
+		decoded.append(value)
+	return decoded
+
+# tournament selection
+def selection(pop, scores, k=3):
+	# first random selection
+	selection_ix = randint(len(pop))
+	for ix in randint(0, len(pop), k-1):
+		# check if better (e.g. perform a tournament)
+		if scores[ix] < scores[selection_ix]:
+			selection_ix = ix
+	return pop[selection_ix]
+
+# crossover two parents to create two children
+def crossover(p1, p2, r_cross):
+	# children are copies of parents by default
+	c1, c2 = p1.copy(), p2.copy()
+	# check for recombination
+	if rand() < r_cross:
+		# select crossover point that is not on the end of the string
+		pt = randint(1, len(p1)-2)
+		# perform crossover
+		c1 = p1[:pt] + p2[pt:]
+		c2 = p2[:pt] + p1[pt:]
+	return [c1, c2]
+
+# mutation operator
+def mutation(bitstring, r_mut):
+	for i in range(len(bitstring)):
+		# check for a mutation
+		if rand() < r_mut:
+			# flip the bit
+			bitstring[i] = 1 - bitstring[i]
+
+# genetic algorithm
+def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
+	# initial population of random bitstring
+	pop = [randint(0, 2, n_bits*len(bounds)).tolist() for _ in range(n_pop)]
+	# keep track of best solution
+	best, best_eval = 0, objective(decode(bounds, n_bits, pop[0]))
+	# enumerate generations
+	for gen in range(n_iter):
+		# if best_eval != 0  and best_eval < 5:
+		# 	break
+		# decode population
+		decoded = [decode(bounds, n_bits, p) for p in pop]
+		# evaluate all candidates in the population
+		scores = [objective(d) for d in decoded]
+		# check for new best solution
+		for i in range(n_pop):
+			if scores[i] < best_eval:
+				best, best_eval = pop[i], scores[i]
+				print(">%d, new best f(%s) = %f" % (gen,  decoded[i], scores[i]))
+		# select parents
+		selected = [selection(pop, scores) for _ in range(n_pop)]
+		# create the next generation
+		children = list()
+		for i in range(0, n_pop, 2):
+			# get selected parents in pairs
+			p1, p2 = selected[i], selected[i+1]
+			# crossover and mutation
+			for c in crossover(p1, p2, r_cross):
+				# mutation
+				mutation(c, r_mut)
+				# store for next generation
+				children.append(c)
+		# replace population
+		pop = children
+	return [best, best_eval]
+
+# define range for input
+bounds = [[-2.0, 5.0], [-2.0, 5.0]]
+# define the total iterations
+n_iter = 500
+# bits per variable
+n_bits = 16
+# define the population size
+n_pop = 100
+# crossover rate
+r_cross = 0.9
+# mutation rate
+r_mut = 1.0 / (float(n_bits) * len(bounds))
+# perform the genetic algorithm search
+best, score = genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut)
+print('Done!')
+decoded = decode(bounds, n_bits, best)
+print('f(%s) = %f' % (decoded, score))
+plt.scatter(X_test,y_test, label='target')
+plt.scatter(X_test,[np.polyval(decoded, x) for x in X_test], label='predicted')
+plt.legend()
+plt.show()
